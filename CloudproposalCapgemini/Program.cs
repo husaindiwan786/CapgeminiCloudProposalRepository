6341da73b9b668b116e5840741ee3d1f15818d88
Husain Diwan <husain.diwan@capgemini.com>
2018-07-30 06:31:51 +0200

    test

---
+++ CloudproposalCapgemini/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudproposalCapgemini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
